61ce769ed9614ae8e8a056de5245ec36ca267c73
minjeongpa <qkralswjdqkd@gmail.com>
2026-03-18 07:01:00 +0000

    2026-03-18 : 백엔드 수정 및 Neo4j 라우터/서비스 추가

--- gpu_backend/app/main.py
+++ gpu_backend/app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, BackgroundTasks, Depends, WebSocket
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routers import health, ai_router, chat_router, train_router, infer_router, search_router, project_router, patent_router
+from app.api.routers import health, ai_router, chat_router, train_router, infer_router, search_router, project_router,query_router, neo4j_router
 
 
 # 임베딩 함수
 from app.utils.embedding import get_embedding 
 from contextlib import asynccontextmanager
-from app.api.routers import health, ai_router, chat_router, train_router, infer_router, search_router, project_router, query_router
 
 from tqdm.auto import tqdm
 import os
@@ -58,7 +57,7 @@ app = FastAPI(title="ipforce_next API", version="0.1.0", lifespan=lifespan)
 origins = [
     "http://localhost:3000",
     "http://frontend:3000",
-    "http://192.168.1.20:3000",
+    "http://192.168.1.149:3000",
     "http://175.118.126.24",
 ]
 
@@ -79,8 +78,10 @@ app.include_router(train_router.router, prefix="/gpu")
 app.include_router(infer_router.router, prefix="/gpu")
 
 app.include_router(search_router.router, prefix="/gpu")
-app.include_router(patent_router.router, prefix="/gpu")
+# app.include_router(patent_router.router, prefix="/gpu")
 app.include_router(query_router.router, prefix="/gpu")
 app.include_router(project_router.router, prefix="/gpu")
 
 app.include_router(health.router, prefix="/gpu")
+
+app.include_router(neo4j_router.router, prefix="/gpu")
