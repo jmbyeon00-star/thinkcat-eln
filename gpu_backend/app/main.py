b6694d926e58e2aa5f294e5b98e43095d60d66a6
박민정 <mjpark@anyfive.com>
2026-03-23 13:33:52 +0900

    main.py 편집

--- gpu_backend/app/main.py
+++ gpu_backend/app/main.py
@@ -57,7 +57,7 @@ app = FastAPI(title="ipforce_next API", version="0.1.0", lifespan=lifespan)
 origins = [
     "http://localhost:3000",
     "http://frontend:3000",
-    "http://192.168.1.149:3000",
+    "http://192.168.1.20:3000",
     "http://175.118.126.24",
 ]
 
